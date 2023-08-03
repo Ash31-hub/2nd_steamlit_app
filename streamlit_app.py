d9c2dca2367246559e910d2b695fbdbdf4c4f3b4
Ash31-hub <126770345+Ash31-hub@users.noreply.github.com>
2023-08-03 18:03:06 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
